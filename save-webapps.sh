c2aeb6bb0c8f98d3d37b4a22b7034b7a12a6383d
Peter Hancock <peter.hancock@gmail.com>
2014-03-04 12:40:40 +0000

    update

--- save-webapps.sh
+++ save-webapps.sh
@@ -1,3 +1,11 @@
-echo -e '/*\n/*/\n!/webapps/\n/webapps/*/' > .gitignore
-git init && git commit --allow-empty -m 'init' && git add webapps/*.war && git commit -m 'wars'
+#!/bin/bash
+
+git status
+
+if [[ "$?" != "0" ]]
+    echo -e '/*\n/*/\n!/webapps/\n/webapps/*/' > .gitignore
+    git init && git commit --allow-empty -m 'init'
+fi
+
+git add webapps/*.war && git commit -m 'wars'
 
